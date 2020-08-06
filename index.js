c9e96eff484c115ff6199944ccc77e40c7a09ceb
unknown <egarcia981@yahoo.com>
2020-08-05 19:42:19 -0700

    second commit

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function counter2() {
 }
 
 
-// Answer
+/// Answer
 
 // "Counter1 code" depicts the best example of a closure. A closure is the combination of a function
 //  bundled together (enclosed) with references to its surrounding state (the lexical environment). 
